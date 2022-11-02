224df549e136c25a8cd94b2ef1e7d2ea5ab42bb0
safiulinrm <zloyangel666999@gmail.com>
2022-11-02 16:46:17 +0300

    fix: "изменил запрос"

--- stats-server/src/main/java/stats/service/HitRepository.java
+++ stats-server/src/main/java/stats/service/HitRepository.java
@@ -9,15 +9,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface HitRepository extends JpaRepository<Hit, Long> {
-    @Query("select e.app, e.uri, count(distinct e.ip)  from Hit e " +
-    "where e.timestamp between ?1 and ?2 and e.uri in ?3")
+    @Query("select new stats.model.ViewStats(e.app, e.uri, count(distinct e.ip)) " +
+            "from Hit e " +
+            "where e.timestamp between :start and :end and e.uri in (:uris) " +
+            "group by e.app, e.uri")
     List<ViewStats> getStatsUnique(LocalDateTime start,
                                    LocalDateTime end,
                                    List<String> uris);
 
-    @Query("select e.app, e.uri, count(e.ip)  from Hit e " +
-            "where e.timestamp between ?1 and ?2 and e.uri in ?3")
+    @Query("select new stats.model.ViewStats(e.app, e.uri, count(e.ip)) " +
+            "from Hit e " +
+            "where e.timestamp between :start and :end and e.uri in (:uris) " +
+            "group by e.app, e.uri")
     List<ViewStats> getStats(LocalDateTime start,
-                                   LocalDateTime end,
-                                   List<String> uris);
+                             LocalDateTime end,
+                             List<String> uris);
 }
