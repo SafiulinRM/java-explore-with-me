5f5fb2920e42a3ab9dad10b0c770fcd4b7c5b9eb
safiulinrm <zloyangel666999@gmail.com>
2022-11-02 16:48:26 +0300

    fix: "поменял аннотации"

--- stats-server/src/main/java/stats/model/ViewStats.java
+++ stats-server/src/main/java/stats/model/ViewStats.java
@@ -1,10 +1,9 @@
 package stats.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ViewStats {
