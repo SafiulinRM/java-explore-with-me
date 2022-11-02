1bf9f4a5fbf6182764d2b38e0ec87a9bb238691d
safiulinrm <zloyangel666999@gmail.com>
2022-11-02 16:47:33 +0300

    fix: "переставил аргументы"

--- stats-server/src/main/java/stats/service/StatsController.java
+++ stats-server/src/main/java/stats/service/StatsController.java
@@ -26,8 +26,8 @@ public class StatsController {
     @GetMapping("/stats")
     public List<ViewStats> getStats(@RequestParam String start,
                                     @RequestParam String end,
-                                    @RequestParam(required = false, defaultValue = "false") Boolean unique,
-                                    @RequestParam List<String> uris) {
+                                    @RequestParam List<String> uris,
+                                    @RequestParam(required = false, defaultValue = "false") Boolean unique) {
         var stats = statsService.getStats(start, end, uris, unique);
         log.info("Статистика собрана");
         return stats;
